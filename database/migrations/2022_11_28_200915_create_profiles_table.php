9bf3cfcd84c9061be5a9cecfeda988099519a3b5
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-11-28 21:23:47 +0100

    create profiles table

---
+++ database/migrations/2022_11_28_200915_create_profiles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use League\CommonMark\Extension\Table\Table;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstName', 255);
+            $table->string('lastName', 255);
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
