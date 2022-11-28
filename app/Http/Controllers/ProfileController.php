3eb1e16b6222e78dfb49189b013e882f14011768
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-11-28 22:45:08 +0100

    create update profime controller method

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Profile;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
@@ -27,6 +28,36 @@ class ProfileController extends Controller
                 'lastname' => $profile->lastName,
                 'id' => $user->id
             ]
+        ], 200);
+    }
+
+    /**
+     * update user profile data
+     * 
+     * @param Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'firstName' => 'required|string',
+            'lastName' => 'required|string',
+        ]);
+
+        if($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+        
+        $userProfile = Profile::find(Auth::user()->id);
+
+        $userProfile->firstName = $request->firstName;
+        $userProfile->lastName = $request->lastName;
+
+        $userProfile->save();
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Successfully update user profile data',
         ], 200);
     }
 }
