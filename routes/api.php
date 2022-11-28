a061d996bbe741de205f44e406c667bf63ff4c3b
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-11-28 22:19:59 +0100

    integrate profile read route

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,8 @@ use Illuminate\Support\Facades\Route;
 */
 Route::prefix('v1')->group(function() {
     Route::post('/login', [AuthController::class, 'login']);
+});
+
+Route::middleware('auth:api')->prefix('v1')->group(function() {
+    Route::post('/profile', [ProfileController::class, 'read']);
 });
